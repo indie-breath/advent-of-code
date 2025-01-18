f666b25d50ae98e3857e1b9dbee95c03386ee7fb
indiebreath <indiebreath@proton.me>
2025-01-19 10:08:24 +1100

    Day 3, Part 2 complete

---
+++ 2024/day-3/task-2.java
@@ -0,0 +1,66 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main(String[] args) {
+        String output = "";
+
+        try {
+            File file = new File("input.txt");
+            Scanner reader = new Scanner(file);
+
+            while (reader.hasNextLine()) {
+                String data = reader.nextLine();
+                output += data;
+            }
+
+            reader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occured");
+            e.printStackTrace();
+        }
+
+        String reg = "(?=(do\\(\\)|don't\\(\\)))";
+        String[] splitOutput = output.split(reg);
+        ArrayList<String> expressions = new ArrayList<String>();
+
+        for (String x : splitOutput) {
+            if (x.startsWith("don't()")) {
+                continue;
+            }
+
+            Pattern regex = Pattern.compile("mul\\([0-9]+,[0-9]+\\)");
+            Matcher matcher = regex.matcher(x);
+
+            while (matcher.find()) {
+                expressions.add(matcher.group(0));
+            }
+        }
+
+        Pattern regex = Pattern.compile("[0-9]+");
+        Integer result = 0;
+
+        for (String x : expressions) {
+            Matcher matcher = regex.matcher(x);
+            Integer num1 = 0;
+            Integer num2 = 0;
+
+            if (matcher.find()) {
+                num1 = Integer.parseInt(matcher.group(0));
+            }
+            if (matcher.find()) {
+                num2 = Integer.parseInt(matcher.group(0));
+            }
+
+            Integer tmp = num1 * num2;
+            result += tmp;
+        }
+
+        System.out.println(result);
+    }
+}
